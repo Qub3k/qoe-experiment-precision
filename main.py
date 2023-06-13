5c6be793975a69e54c1a9c93e5b161f5218b0f2d
Lucjan Janowski <janowski@kt.agh.edu.pl>
2023-06-14 00:14:52 +0200

    Adding $g$ metric to the file

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ from scipy import stats
 from scipy import linalg
 from scipy.optimize import curve_fit
 
+import random
+from typing import NamedTuple
+
+import jax
+import jax.numpy as jnp
+import jax.tree_util as tree
+from jax.scipy.special import betaln
 
 ################### start of $\ell$ metric #############################
 def one_or_nan(x):
@@ -272,29 +279,234 @@ def compar_by_a(data_frame, experiment_id, score, pvs, subject):
 
 ################ start metric $g$ ################################
 
+def logbinom(n, k):
+    '''
+    log of `n choose k`
 
+    '''
+    return -jnp.log1p(n) - betaln(n - k + 1., k + 1.)
+
+
+def vmin(psi: jnp.ndarray) -> jnp.ndarray:
+    return (jnp.ceil(psi) - psi) * (psi - jnp.floor(psi))
+
+
+def vmax(psi: jnp.ndarray) -> jnp.ndarray:
+    return (psi - 1.) * (5 - psi)
+
+
+def _C(Vmax, Vmin):
+    return 3. / 4. * Vmax / (Vmax - Vmin)
+
+
+def _gsd_log_prob(psi, rho, k):
+    index = jnp.arange(0, 6)
+
+    Vmin = vmin(psi)
+    Vmax = vmax(psi)
+    C = _C(Vmax, Vmin)
+    beta_bin_part = logbinom(4, k - 1.) + jnp.sum(
+        jnp.where(
+            index <= k - 2,
+            jnp.log((psi - 1) * rho / 4 + index * (C - rho)),
+            0.
+        )
+    ) + jnp.sum(
+        jnp.where(
+            index <= 4 - k,
+            jnp.log((5. - psi) * rho / 4 + index * (C - rho)),
+            0.
+        )
+    ) - jnp.sum(
+        jnp.where(
+            index <= 3,
+            jnp.log(rho + index * (C - rho)),
+            0.
+        )
+    )
+
+    min_var_part = jax.nn.relu(1. - jnp.abs(k - psi))
+    log_min_var_part = jnp.where(rho < C, 0., jnp.log(rho - C)) - \
+                       jnp.log1p(-C) + jnp.log(min_var_part)
+    log_bin_part = jnp.log1p(-rho) - jnp.log1p(-C) + logbinom(4, k - 1.) + \
+                   (k - 1) * (jnp.log(psi - 1) - jnp.log(4)) + \
+                   (5 - k) * (jnp.log(5 - psi) - jnp.log(4))
+
+    almost_neg_inf = np.log(1e-10)
+    logmix = jnp.logaddexp(jnp.where(min_var_part == 0, almost_neg_inf, log_min_var_part), log_bin_part)
+
+    logmix = jnp.where(min_var_part == 0, log_bin_part, logmix)
+    return jnp.where(rho < C, beta_bin_part, logmix)
+
+
+class GSDParams(NamedTuple):
+    psi: float
+    rho: float
+
+
+@jax.jit
+def fit_moments(data: jnp.ndarray) -> GSDParams:
+    psi = jnp.dot(data, jnp.arange(1, 6)) / jnp.sum(data)
+    V = jnp.dot(data, jnp.arange(1, 6) ** 2) / jnp.sum(data) - psi ** 2
+    return GSDParams(psi=psi, rho=(vmax(psi) - V) / (vmax(psi) - vmin(psi)))
+
+
+v_gsd_log_prob = jax.vmap(_gsd_log_prob, (None, None, 0), (0))
+
+
+class _OptState(NamedTuple):
+    params: GSDParams
+    prevoious_params: GSDParams
+    count: int
+
+
+def fit_grad_bogdan(data: jnp.ndarray, max_iterations=100) -> GSDParams:
+    def ll(theta: GSDParams) -> float:
+        logits = v_gsd_log_prob(theta.psi, theta.rho, jnp.arange(1, 6))
+        return jnp.dot(data, logits) / jnp.sum(data)
+
+    grad_ll = jax.grad(ll)
+    theta0 = fit_moments(data)
+    rate = jnp.concatenate([jnp.zeros((1,)), jnp.logspace(-15, 5, 20, base=2.)])
+
+    def update(tg, t, lo, hi):
+        '''
+
+        :param tg: gradient
+        :param t: theta parametry
+        :param lo: low bound
+        :param hi: upper bound
+        :return: updated params
+        '''
+        nt = t + rate * jnp.where(jnp.isnan(tg), 0., tg)
+        mask = jnp.logical_and(nt > lo, nt < hi)
+        return jnp.where(mask, nt, nt[0])
+
+    # TODO handle special cases
+    lo = GSDParams(psi=1., rho=0.)
+    hi = GSDParams(psi=5., rho=1.)
+
+    def body_fun(state: _OptState) -> _OptState:
+        t, _, count = state
+        g = grad_ll(t)
+        new_theta = tree.tree_map(update, g, t, lo, hi)
+        new_lls = jax.vmap(ll)(new_theta)
+        max_idx = jnp.argmax(jnp.where(jnp.isnan(new_lls), -jnp.inf, new_lls))
+        return _OptState(params=jax.tree_map(lambda t: t[max_idx], new_theta),
+                         prevoious_params=t,
+                         count=count + 1
+                         )
+
+    def cond_fun(state: _OptState) -> bool:
+        tn, tnm1, c = state
+        should_stop = jnp.logical_or(jnp.all(jnp.array(tn) == jnp.array(tnm1)), c > max_iterations)
+        # stop on NaN
+        should_stop = jnp.logical_or(should_stop, jnp.any(jnp.isnan(jnp.array(tn))))
+        return jnp.logical_not(should_stop)
+
+    opt_state = jax.lax.while_loop(cond_fun, body_fun,
+                                   _OptState(params=theta0,
+                                             prevoious_params=jax.tree_map(lambda _: jnp.inf, theta0),
+                                             count=0
+                                             )
+                                   )
+    return opt_state
+
+
+def precision_g(scores):
+    """
+    Estimates the SOS parameter a and its standard error for a subjective experiment.
+
+    Parameters
+    ----------
+    scores : n x k matrix of user ratings in one experiment with n subjects and k stimuli / test conditions
+        An experiment is described by the matrix of user ratings (for n subjects and for k stimuli). Row i reflects ratings for stimuli i.
+
+    Returns
+    -------
+    sosa : float
+        Estimate of the SOS parameter a.
+    stderr : float
+        Standard error of the SOS parameter estimation.
+    n : integer
+        Number of test conditions (i.e. data points) of the estimation
+
+    """
+    n, _ = scores.shape
+    rho = np.zeros(n)
+    for i in range(n):
+        scores_tmp = np.array([np.sum(scores[i,:] == 1), np.sum(scores[i,:] == 2), np.sum(scores[i,:] == 3),
+                 np.sum(scores[i,:] == 4), np.sum(scores[i,:] == 5)])
+        gsd_hat = fit_grad_bogdan(scores_tmp)
+        rho[i] = gsd_hat.params[1]
+    g = np.nanmean(rho)
+    stdg = np.nanstd(rho)
+    n = len(rho)
+    return {
+        'g': g,
+        'std': stdg,
+        'n': n
+    }
+
+
+def compar_by_g(data_frame, experiment_id, score, pvs, subject):
+    """
+    Compare two (exactly two) experiments
+    :param data_frame: input data frame with long data structure. This DF has to contain at least four columns, experiment id, score, pvs, and subject
+    :param experiment_id: name of the column with experiment ID
+    :param score: name of the column with score
+    :param pvs: name of the column with PVS ID
+    :param subject: name of the column with subject ID
+    :return: dictionary with:
+      t_stat: is t-test statistics
+      p_value: is t-test p-value
+    """
+    experiments = np.unique(data_frame[experiment_id])
+    if len(experiments) != 2:
+        return {
+            't_stat': -1,
+            'p_value': -1
+        }
+    else:
+        df_pivot = df[df[experiment_id] == experiments[0]].pivot(index=pvs, columns=subject, values=score)
+        matrix_1 = df_pivot.values
+        df_pivot = df[df[experiment_id] == experiments[1]].pivot(index=pvs, columns=subject, values=score)
+        matrix_2 = df_pivot.values
+        prec_1 = precision_g(matrix_1)
+        prec_2 = precision_g(matrix_2)
+        t_stat, p_value = stats.ttest_ind_from_stats(prec_1['g'], prec_1['std'], prec_1['n'],
+                                                     prec_2['g'], prec_2['std'], prec_2['n'],
+                                                     equal_var=False)
+
+        return {
+            't_stat': t_stat,
+            'p_value': p_value
+        }
 
 ################# end of $g$ metric ################################
 
 if __name__ == '__main__':
     # Example how to calculate l metric for a matrix of scores (rows are PVSs and columns are subjects)
-    scores = np.ceil(5*np.random.uniform(0, 1, size = [100, 24]))
+    scores = np.ceil(5*np.random.uniform(0, 1, size = [10, 24]))
     a = precision_l(scores)
     print(a['l'])
+    g = precision_g(scores)
+    print(g)
 
     print(precision_a(scores))
 
     # Example how to use comparison metrix l for comparing two experiments
     # Generate data
+    n = 200
     # Two different experiments
-    exp = np.repeat([0, 1], 1000)
+    exp = np.repeat([0, 1], int(n/2))
     # 20 subjects per experiment
-    subject = np.tile(np.arange(1, 21), 100)
+    subject = np.tile(np.arange(1, 21), int(n/20))
     # 100 PVS per experiment
-    pvs = np.repeat(np.arange(100), 20)
+    pvs = np.repeat(np.arange(10), int(n/10))
     np.random.seed(0)  # For reproducibility
     # random score per PVS and subject
-    score = np.random.choice(np.arange(1, 6), size=2000)
+    score = np.random.choice(np.arange(1, 6), size=n)
 
     # Create DataFrame, we assume this DataFrame is read from files/databases/etc.
     df = pd.DataFrame({'exp': exp, 'pvs': pvs, 'subject': subject, 'score': score})
@@ -311,4 +523,4 @@ if __name__ == '__main__':
     print(precision_l(matrix)['l'])
     print(compar_by_l(df, 'exp', 'score', 'pvs', 'subject'))
     print(compar_by_a(df, 'exp', 'score', 'pvs', 'subject'))
-
+    print(compar_by_g(df, 'exp', 'score', 'pvs', 'subject'))
